0bb790be7e9d40de417bce30bda1f791589fbe23
VITORIO AUGUSTO DIAS DOS SANTOS <vitorio.adsantos@senacsp.edu.br>
2026-04-16 20:22:30 -0300

    primeiro commit

---
+++ sofa_score_brasil.java
@@ -0,0 +1,27 @@
+public class sofa_score_brasil {
+    public static void main(String[] args) {
+        String[] partidas = {
+                "Flamengo:3:1:Palmeiras",
+                "Corinthians:0:0:São Paulo",
+                "Atletico-MG:2:2:Fluminense",
+                "Palmeiras:1:0:Corinthians",
+                "São Paulo:3:2:Flamengo",
+                "Fluminense:0:1:Atletico-MG",
+                "Flamengo:2:0:Corinthians",
+                "Palmeiras:4:1:Fluminense",
+                "São Paulo:0:0:Atletico-MG",
+                "Corinthians:1:3:Fluminense",
+                "Atletico-MG:0:2:Flamengo",
+                "Fluminense:1:1:São Paulo"
+        };
+
+        public static Partida parserPartida(String linha) {
+            return null;
+        }
+        
+            
+       
+
+
+    }
+}
