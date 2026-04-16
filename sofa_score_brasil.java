98a3d070998c832829e4725d66dcf83f62696b2c
Vitorio <vito.augustodias@gmail.com>
2026-04-16 20:27:38 -0300

    Tarefa1

--- sofa_score_brasil.java
+++ sofa_score_brasil.java
@@ -15,9 +15,27 @@ public class sofa_score_brasil {
                 "Fluminense:1:1:São Paulo"
         };
 
-        public static Partida parserPartida(String linha) {
+        public static String[] parsearPartida(String linha) {
+        
+        String[] partes = linha.split(":");
+        
+        if (partes.length != 4) {
+            System.out.println("[ERRO] Linha inválida: " + linha);
             return null;
         }
+
+        String timeCasa = partes[0].trim().toUpperCase();
+        String golsCasa = partes[1].trim();
+        String golsFora = partes[2].trim();
+        String timeFora = partes[3].trim().toUpperCase();
+
+        if (timeCasa.isEmpty() || timeFora.isEmpty() || golsCasa.isEmpty() || golsFora.isEmpty()) {
+            System.out.println("[ERRO] Linha inválida: " + linha);
+            return null;
+        }
+
+        return new String[]{timeCasa, golsCasa, golsFora, timeFora};
+    }
         
             
        
